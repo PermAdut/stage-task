0c044e78a55661d2286f6e4a459e337f3ded96a1
PermAdut <hdvhdvhdvwww@gmail.com>
2025-07-08 11:16:05 +0300

    fix: resolved bugs according to code review

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,12 +1,3 @@
-export const navbarObjects = [
-  'why spring',
-  'learn',
-  'projects',
-  'academy',
-  'community',
-  'tanzu spring',
-]
-
 export const projectArray = [
   {
     imageLink: './images/spring-boot.svg',
@@ -61,5 +52,92 @@ export const projectArray = [
       'Protects your application with comprehensive and extensible authentication and authorization support.',
     version: '6.5.1',
     moreVersion: '+ 10 versions',
+  },
+]
+
+export const navbarObjects = [
+  {
+    title: 'why spring',
+    isArrow: true,
+    subMenuObjects: [
+      { text: 'Overview', isLink: true, isMargin: false },
+      { text: 'Generative AI', isLink: true, isMargin: false },
+      { text: 'Microservices', isLink: true, isMargin: false },
+      { text: 'Reactive', isLink: true, isMargin: false },
+      { text: 'Event Driven', isLink: true, isMargin: false },
+      { text: 'Cloud', isLink: true, isMargin: false },
+    ],
+  },
+  {
+    title: 'learn',
+    isArrow: true,
+    subMenuObjects: [
+      { text: 'Overview', isLink: true, isMargin: false },
+      { text: 'Quickstart', isLink: true, isMargin: false },
+      { text: 'Guides', isLink: true, isMargin: false },
+      { text: 'Blog', isLink: true, isMargin: false },
+      { text: 'Security Advisores', isLink: true, isMargin: false },
+    ],
+  },
+  {
+    title: 'projects',
+    isArrow: true,
+    subMenuObjects: [
+      { text: 'Overview', isLink: true, isMargin: false },
+      { text: 'Spring Boot', isLink: true, isMargin: false },
+      { text: 'Spring Framework', isLink: true, isMargin: false },
+      { text: 'Spring Cloud', isLink: true, isMargin: false },
+      { text: 'Spring Cloud Data Flow', isLink: true, isMargin: false },
+      { text: 'Spring Data', isLink: true, isMargin: false },
+      { text: 'Spring Integration', isLink: true, isMargin: false },
+      { text: 'Spring Batch', isLink: true, isMargin: false },
+      { text: 'Spring Security', isLink: true, isMargin: false },
+      { text: 'Spring AI', isLink: true, isMargin: true },
+      { text: 'Release Calendar', isLink: true, isMargin: false },
+      { text: 'Security advisories', isLink: true, isMargin: true },
+      { text: 'DEVELOPMENT TOOLS', isLink: false, isMargin: false },
+      { text: 'Spring Tools', isLink: true, isMargin: false },
+      {
+        text: 'Spring Initializr',
+        isLink: true,
+        isMargin: false,
+        extraContent: `<svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="arrow-up-right-from-square"
+                        style="height: 1em; margin-left: 0.5rem; vertical-align: -0.125rem;"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"
+                        ></path></svg>`,
+      },
+    ],
+  },
+  {
+    title: 'academy',
+    isArrow: true,
+    subMenuObjects: [
+      { text: 'Courses', isLink: true, isMargin: false },
+      { text: 'Get Certified', isLink: true, isMargin: false },
+    ],
+  },
+  {
+    title: 'community',
+    isArrow: true,
+    subMenuObjects: [
+      { text: 'Overview', isLink: true, isMargin: false },
+      { text: 'Events', isLink: true, isMargin: false },
+      { text: 'Authors', isLink: true, isMargin: false },
+    ],
+  },
+  {
+    title: 'tanzu spring',
+    isArrow: false,
+    subMenuObjects: undefined,
   },
 ]
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,30 +1,90 @@
 import { navbarObjects } from './constants.js'
-const navbarList = document.querySelectorAll('.navbar_item')
-Array.from(navbarList).forEach((el, index) => {
-  el.innerHTML = navbarObjects[index]
-})
-const navbarSubItemList = document.querySelectorAll('.navbar_subitem_title')
-Array.from(navbarSubItemList).forEach((el, index) => {
-  el.innerHTML = navbarObjects[index]
-})
-const navbarSubItem = document.querySelectorAll('.navbar_subitem')
-const navbarSubItemTitle = document.querySelectorAll('.navbar_subitem_title')
-Array.from(navbarSubItem).forEach((el, index) => {
-  navbarSubItemTitle[index].addEventListener('click', () => {
-    const navbarSubitemTitle = el.querySelector('.navbar_subitem_title')
-    navbarSubitemTitle.classList.toggle('navbar_subitem_title_active')
-    const navbarSubitemsLinks = document.querySelectorAll(
-      '.navbar_subitem_links'
-    )
-    Array.from(navbarSubitemsLinks).forEach((el) => {
-      el.classList.add('navbar_subitem_links')
-    })
-    const elementNavbarSubitemLink = el.querySelector('ul')
-    if (elementNavbarSubitemLink) {
-      elementNavbarSubitemLink.classList.toggle('navbar_subitem_links')
+
+function generateNavBarItems() {
+  const navbarItems = document.getElementsByClassName('navbar_items')
+  navbarObjects.forEach((el, index) => {
+    let div = document.createElement('div')
+    div.className = 'navbar_link'
+    navbarItems[0].append(div)
+    const title = generateTitle(el.title)
+    const subItems = generateSubItemsList(el.subMenuObjects)
+    const link = document.querySelectorAll('.navbar_link')
+    link[index].innerHTML = `${title}${subItems}`
+    const span = div.querySelector('.navbar_item')
+    if (span && el.isArrow) {
+      span.style.position = 'relative'
+      const afterElement = document.createElement('span')
+      afterElement.style.content = '" "'
+      afterElement.style.right = '15px'
+      afterElement.style.top = '20px'
+      afterElement.style.width = '0.5rem'
+      afterElement.style.height = '0.5rem'
+      afterElement.style.borderBottom = '2px solid var(--text-color)'
+      afterElement.style.borderLeft = '2px solid var(--text-color)'
+      afterElement.style.borderRadius = '1px'
+      afterElement.style.transform = 'rotate(-45deg)'
+      afterElement.style.marginLeft = '0.5rem'
+      afterElement.style.marginTop = '-0.1rem'
+      span.appendChild(afterElement)
     }
   })
-})
+}
+
+function generateNavBarSubItems() {
+  const navbarItems = document.getElementsByClassName('navbar_subitems')
+  navbarObjects.forEach((el) => {
+    let div = document.createElement('div')
+    div.className = 'navbar_subitem'
+    navbarItems[0].append(div)
+    let secondDiv = document.createElement('div')
+    secondDiv.className = 'navbar_subitem_extra'
+    div.appendChild(secondDiv)
+    const title = generateTitle(el.title, 'navbar_subitem_title')
+    secondDiv.innerHTML = `${title}`
+    const span = div.querySelector('.navbar_subitem_title')
+    if (span && el.isArrow) {
+      span.style.position = 'relative'
+      const afterElement = document.createElement('span')
+      afterElement.style.content = '" "'
+      afterElement.style.position = 'absolute'
+      afterElement.style.right = '15px'
+      afterElement.style.top = '20px'
+      afterElement.style.width = '15px'
+      afterElement.style.height = '15px'
+      afterElement.style.border = 'solid #98afae'
+      afterElement.style.borderWidth = '0 2px 2px 0'
+      afterElement.style.display = 'block'
+      afterElement.style.transform =
+        'rotate(-135deg) translate(5px, 15px) scale(-1)'
+      afterElement.style.transition = 'transform 0.2s ease-out'
+      span.appendChild(afterElement)
+    }
+  })
+}
+
+function generateTitle(title, className = 'navbar_item') {
+  return `<span class="${className}">${title}</span>`
+}
+
+function generateSubItemsList(
+  subMenuObjects,
+  ulClassName = 'navbar_dropdown_content',
+  linkClassName = 'navbar_dropdown_link'
+) {
+  if (!subMenuObjects) return ''
+  let result = `<ul class=${ulClassName}>`
+  subMenuObjects.forEach((el) => {
+    if (el.isMargin)
+      result += `<li style="margin-bottom: 24px"><a class=${linkClassName} href="#">${el.text}</a></li>`
+    else if (!el.isLink)
+      result += `<li><div class=${linkClassName}>${el.text}</div></li>`
+    else
+      result += `<li><a class=${linkClassName}>${el.text}${el?.extraContent ? el.extraContent : ''}</a></li>`
+  })
+  result += `</ul>`
+  return result
+}
+
 let isToogeled = false
 const navbarBurger = document.querySelector('.navbar_burger')
 navbarBurger.addEventListener('click', () => {
@@ -47,4 +107,46 @@ navbarBurger.addEventListener('click', () => {
       el.classList.toggle('navbar_burger_line_active')
     })
   }
+})
+
+generateNavBarItems()
+generateNavBarSubItems()
+const navbarSubItem = document.querySelectorAll('.navbar_subitem_extra')
+const navbarTitles = document.querySelectorAll('.navbar_subitem_title')
+Array.from(navbarSubItem).forEach((el, index) => {
+  el.addEventListener('click', () => {
+    const subItems = generateSubItemsList(
+      navbarObjects[index].subMenuObjects,
+      'navbar_subitem_links',
+      'navbar_subitem_link'
+    )
+    navbarTitles.forEach((element, indexEl) => {
+      if (element.firstElementChild && index === indexEl) {
+        element.firstElementChild.classList.toggle(
+          'navbar_subitem_title_active'
+        )
+      } else if (element.firstElementChild)
+        element.firstElementChild.classList.remove(
+          'navbar_subitem_title_active'
+        )
+    })
+    const existedSubItems = document.querySelectorAll('.navbar_subitem_links')
+    existedSubItems.forEach((item) => {
+      if (item !== el.nextSibling) {
+        item.remove()
+      }
+    })
+
+    if (
+      el.nextSibling &&
+      el.nextSibling.classList.contains('navbar_subitem_links')
+    ) {
+      el.nextSibling.remove()
+    } else {
+      const tempDiv = document.createElement('div')
+      tempDiv.innerHTML = subItems
+      const subItemsElement = tempDiv.firstElementChild
+      el.parentNode.insertBefore(subItemsElement, el.nextSibling)
+    }
+  })
 })
