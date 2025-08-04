43a626ec595238ee6c8ccdbb49eb834d27c94775
PermAdut <hdvhdvhdvwww@gmail.com>
2025-08-04 11:53:04 +0300

    refactor: refactored auth logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router";
 import MainPage from "./pages/MainPage/MainPage";
 import Page404 from "./pages/Page404/Page404";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import AuthProtectedPage from "./components/ui/AuthProtectedPage/AuthProtectedPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="*" element={<Page404 />} />
-        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/"
+          element={<AuthProtectedPage children={<MainPage />} />}
+        />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
